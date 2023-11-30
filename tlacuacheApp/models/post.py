b8f855fcad8065ac524ed765634a5698084f22a8
angel <angel1521ramos@gmail.com>
2023-11-29 23:51:55 -0600

    ultimos detalles

--- tlacuacheApp/models/post.py
+++ tlacuacheApp/models/post.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    date = models.CharField(max_length=100)
+    date = models.DateField()
     image = models.TextField()#link
     description = models.TextField()
     url = models.TextField()#link
--- tlacuacheApp/views/posts.py
+++ tlacuacheApp/views/posts.py
@@ -1,22 +1,26 @@
 from django.shortcuts import render, get_object_or_404
 from tlacuacheApp.models import Post
 
-#from twitter.account import Account
+# from twitter.account import Account
 
-#muestra post
+# muestra post
+
+
 def posts(request):
-    #llamamos los posts de la bd
-    posts = Post.objects.all()
+    # llamamos los posts de la bd
+    posts = Post.objects.order_by("-date")
     return render(
         request,
         "posts/posts.html",
         {"posts": posts},
     )
 
-#detalle de post
+# detalle de post
+
+
 def detail_posts(request, posts_id):
-    #llamamos los posts de la bd
-    posts = Post.objects.exclude(pk=posts_id)
+    # llamamos los posts de la bd
+    posts = Post.objects.exclude(pk=posts_id).order_by("-date")[:4]
     post = get_object_or_404(Post, pk=posts_id)
     return render(
         request, "posts/detail.html", {"post": post, "posts": posts}
