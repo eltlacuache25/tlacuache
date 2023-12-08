f9bbf30dc875009980fcd285165a1511dcf0d356
eltlacuache25 <=>
2023-12-08 11:58:26 -0600

    cambios con imagenes

--- tlacuacheApp/models/post.py
+++ tlacuacheApp/models/post.py
@@ -3,6 +3,5 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=100)
     date = models.DateField()
-    image = models.TextField()#link
+    image = models.ImageField(upload_to="images", null=True)
     description = models.TextField()
-    url = models.TextField()#link
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\DIF\Desktop\PROGRAMAS DESARROLLADOS\tlacuache\venv
+set VIRTUAL_ENV=C:\Users\DIF\Pictures\proyectos emilio y andres\tlacuache\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
